d3377de70da95a732e7d2cfd7d4037b954a8ca8c
Chris Griffith <chris.griffith@gmail.com>
2018-12-12 10:58:42 -0800

    Initial Commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor() {}
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+}
